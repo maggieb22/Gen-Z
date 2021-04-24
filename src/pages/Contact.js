5d723f4ef479c88ef0325ff87a5f8ad7ce91ddc5
Sophia Liu <sophiazyliu@github.com>
2021-04-24 07:00:10 -0700

    Add emailing feature on Contact Us form.

---
+++ src/pages/Contact.js
@@ -0,0 +1,90 @@
+class Contact extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          name: '',
+          email: '',
+          subject:'',
+          message: ''
+        }
+    }
+
+    onNameChange(event) {
+        this.setState({name: event.target.value})
+    }
+
+    onEmailChange(event) {
+        this.setState({email: event.target.value})
+    }
+
+    onSubjectChange(event) {
+        this.setState({subject: event.target.value})
+    }
+
+    onMsgChange(event) {
+        this.setState({message: event.target.value})
+    }
+
+    submitEmail(e){
+        e.preventDefault();
+        axios({
+          method: "POST", 
+          url:"/send", 
+          data:  this.state
+        }).then((response)=>{
+          if (response.data.status === 'success'){
+              alert("Message Sent."); 
+              this.resetForm()
+          } else if (response.data.status === 'fail') {
+              alert("Message failed to send.")
+          }
+        })
+    }
+
+    resetForm() {
+        this.setState({name: '', email: '',subject:'', message: ''})
+    }
+
+    render() {
+        return (
+            <div class="my-5">
+                <h2>Contact Us</h2>
+                <Container>
+                    <Row className="justify-content-md-center">
+                    <Col md="9">
+                        <Form id="contact-form" onSubmit={this.submitEmail.bind(this)} 
+                                    method="POST">
+                        <Input 
+                            type="email" 
+                            placeholder="Email:" 
+                            className="my-2" 
+                            aria-describedby="emailHelp"
+                            required value={this.state.email} onChange=
+                            {this.onEmailChange.bind(this)}
+                        />
+                        <Input 
+                            type="text" 
+                            placeholder="Subject:" 
+                            className="my-2"
+                            required value={this.state.subject}
+                            onChange={this.onSubjectChange.bind(this)}
+                        />
+                        <Input 
+                            type="textarea" 
+                            placeholder="Message:" 
+                            className="my-2"
+                            required value={this.state.message}
+                            onChange= {this.onMsgChange.bind(this)}
+                        />
+                        <div class="text-right">
+                            <Button>Submit</Button>
+                        </div>
+                        </Form>
+                    </Col>
+                    </Row>
+                </Container>
+            </div>
+        )
+    }
+}
---
+++ src/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const { ButtonDropdown } = require('reactstrap');
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+    host: "gen-z-project.org", //replace with your email provider // gmail.com
+    port: 587,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD
+    }
+});
+
+// verify connection configuration
+transporter.verify(function(error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Server is ready to take our messages");
+    }
+});
+  
+app.post('/send', (req, res, next) => {
+    var email = req.body.email
+    var subject = req.body.subject
+    var message = req.body.message
+  
+    var mail = {
+      to: maggie@gen-z-project.org, // receiver email,
+      subject: subject,
+      text: message
+    }
+  
+    transporter.sendMail(mail, (err, data) => {
+      if (err) {
+        res.json({
+          status: 'fail'
+        })
+      } else {
+        res.json({
+         status: 'success'
+        })
+      }
+    })
+})
